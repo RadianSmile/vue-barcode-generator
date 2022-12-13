5e1985f427019802db64a83f34b57a351b47cba7
radian <craziestdavid@gmail.com>
2022-12-13 23:40:50 +0800

    m

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,4 @@
- /* eslint-disable import/prefer-default-export */
+
 
 export { default as VueBarcodeGenerator } from './VueBarcodeGenerator.vue';
 
-// Other components you plan to publish go here
--- vite.config.ts
+++ vite.config.ts
@@ -7,27 +7,25 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
 
-  // // to build doc
-  // // https://vitejs.dev/guide/build.html
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/components/index.ts'),
+      name: 'VueBarcodeGenerator',
+      fileName: (format) => `VueBarcodeGenerator.${format}.js`,
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["vue","jsbarcode"],
 
-  // build: {
-  //   lib: {
-  //     entry: path.resolve(__dirname, 'src/components/index.ts'),
-  //     name: 'VueBarcodeGenerator',
-  //     fileName: (format) => `VueBarcodeGenerator.${format}.js`,
-  //   },
-  //   rollupOptions: {
-  //     // make sure to externalize deps that shouldn't be bundled
-  //     // into your library
-  //     external: ["vue","JsBarcode"],
-
-  //     output: {
-  //       // Provide global variables to use in the UMD build
-  //       // for externalized deps
-  //       globals: {
-  //         vue: 'Vue',
-  //       },
-  //     },
-  //   },
-  // }
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+          jsbarcode : 'JsBarcode'
+        },
+      },
+    },
+  }
 })
